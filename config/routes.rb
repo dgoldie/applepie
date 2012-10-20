0e07beb8a296585ce0b23e8399a5627dd7f87f0d
Peter Bloom <peter.bloom@bloombusiness.com>
2012-10-20 15:56:10 -0700

    PSB: about_us

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Applepie::Application.routes.draw do
+
   authenticated :user do
     root :to => 'home#index'
   end
+
   root :to => "static_pages#landing_page"
   devise_for :users
   resources :users
+
+  match '/about_us' => 'static_pages#about_us', :as => :about_us
+
 end
