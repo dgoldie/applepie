56cc78e297db5f7b5b464ff6a1fd2977824a2d31
Doug Goldie <dgoldie15@gmail.com>
2012-10-20 12:17:09 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Applepie::Application.config.secret_token = '7a68d85275ed651a7e8c079df61b0714d0eb5907b6319c49470526e9c0e12ee061140679707589b35a322e1492d05a80ea82ebaa86d21dacd9c0f7a0ee9951ba'
