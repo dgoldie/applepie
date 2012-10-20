4abb4cd6d273f1804ff47b3119239378d891cd74
Doug Goldie <dgoldie15@gmail.com>
2012-10-20 16:52:40 -0700

    update question migration for user association

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,12 +3,7 @@ class Question < ActiveRecord::Base
   
   belongs_to :user
   
-  def author=(user)
-    author = user
-  end
-  def author
-    user
-  end
+
   def display_author
     user.blank? ? "<Anonymous>" : user.email
   end
