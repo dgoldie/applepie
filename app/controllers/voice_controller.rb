8389f76aab6a597e2693b1a49afcb54c1f8000a3
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-10-21 12:48:46 -0700

    fixing voice views 2

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -35,7 +35,7 @@ class VoiceController < ApplicationController
     # email the recording url to the support team via sendhub.net ;)
     puts "recording_url #{recording_url}"
     
-    return "Success"
+    render :nothing
   end
   
   def transcribing
@@ -44,7 +44,7 @@ class VoiceController < ApplicationController
     # puts the transcribing url;)
     puts "transcribing_url #{transcribing_url}"
     
-    return "Success"
+    render :nothing
   end
   
 end
