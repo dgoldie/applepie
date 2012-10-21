745d37b5737b9cc7fd0042c33768b1b54d1bcc0f
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-10-21 16:15:55 -0700

    fix

--- app/controllers/voice_controller.rb
+++ app/controllers/voice_controller.rb
@@ -7,14 +7,14 @@ class VoiceController < ApplicationController
   end
   
   def init_call 
-     
+    puts current_user.to_json
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new(TWILIO_SID, TWILIO_TOKEN)
 
     @account = @client.account
     @call = @account.calls.create({
       :from => '+14242752362',
-      :to => '+1' + current_user.phone, :url => 'http://askadoc.herokuapp.com/voice/make_call',
+      :to => '+18175848815', :url => 'http://askadoc.herokuapp.com/voice/make_call',
       :application_sid => 'AP3418c8bd94908cfd7581e36c052130a6'})
     
     redirect_to questions_path
