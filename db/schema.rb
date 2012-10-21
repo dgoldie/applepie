5dea46938b8065a741af1abecadcc994944b8295
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-10-21 11:30:22 -0700

    schema change?

--- db/schema.rb
+++ db/schema.rb
@@ -113,7 +113,6 @@ ActiveRecord::Schema.define(:version => 20121021033413) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
-    t.string   "phone"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
