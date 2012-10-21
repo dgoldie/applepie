c9d542c6da125849c81ff323c300ae4b61c58146
Andrew Clarkson <andrew@andrewzclarkson.com>
2012-10-21 11:53:07 -0700

    added salutation to user and fixed phone

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121021033413) do
+ActiveRecord::Schema.define(:version => 20121021185104) do
 
   create_table "answers", :force => true do |t|
     t.string   "name"
@@ -113,6 +113,8 @@ ActiveRecord::Schema.define(:version => 20121021033413) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "phone"
+    t.string   "salutation"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
